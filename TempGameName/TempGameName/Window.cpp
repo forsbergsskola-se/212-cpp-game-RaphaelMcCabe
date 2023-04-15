0b1e368ec1f6a774fd923506cc6952306e402a53
Raphael McCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2023-04-15 23:14:11 +0200

    Fixed comment location :runner:

--- TempGameName/TempGameName/Window.cpp
+++ TempGameName/TempGameName/Window.cpp
@@ -1,6 +1,7 @@
 #include "Window.h"
 #include <cstdio>
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 Window::Window(int width, int height) : success{}
 {
@@ -41,17 +42,6 @@ void Window::render(Image& image)
 	SDL_UpdateWindowSurface(window);
 }
 
-// NOT NECESSARY:
-// DO NOT DO: Switch to Textures (Chapter 7)
-// Window constructor: CreateRenderer insteade of GetWindowSurface
-// Window render: replace SDL_BLitSurface with SDL_RenderCOpy and SDL_UPdateWIndowSUrface with SDL_RenderPResent
-// Image: replace imageSurface with newTexture SDL_CreateTextureFromSurface
-
-
-
-
-
-
 
 // STEP 1: Follow Font Tutorial
 // https://lazyfoo.net/tutorials/SDL/16_true_type_fonts/index.php
@@ -59,3 +49,14 @@ void Window::render(Image& image)
 // do the text tutorial, but only do SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
 // do not create a texture
 // instead
+
+
+
+// try to include xxx.h > project settings: additional include directory > additional library directory > linker input (.lib) > dll copy
+
+
+// NOT NECESSARY:
+// DO NOT DO: Switch to Textures (Chapter 7)
+// Window constructor: CreateRenderer insteade of GetWindowSurface
+// Window render: replace SDL_BLitSurface with SDL_RenderCOpy and SDL_UPdateWIndowSUrface with SDL_RenderPResent
+// Image: replace imageSurface with newTexture SDL_CreateTextureFromSurface
